3c795878a16b8c680d747e01e9f1a0f487957bf3
SiTiROS <sitiros666@gmail.com>
2024-06-29 04:27:57 +0600

    142. An Introduction to Inheritance

---
+++ 16C++102-Inheritance/16C++102-Inheritance/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class BaseClass
+{
+protected:
+	void hello() { std::cout << "Hello world" << std::endl; }
+};
+
+class DerivedClass : public BaseClass
+{
+public:
+	void toString()
+	{
+		hello();
+	}
+};
+
+
+int main()
+{
+	// 142. An Introduction to Inheritance
+
+	DerivedClass derived;
+	derived.toString();
+
+
+	std::cout << "END";
+	return 0;
+}
