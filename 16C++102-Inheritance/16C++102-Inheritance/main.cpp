72706d15fecef253e8bd7fa2b6dc02a8f96b6122
SiTiROS <sitiros666@gmail.com>
2024-06-30 03:47:06 +0600

    145. Redefining Base Class Member Functions

--- 16C++102-Inheritance/16C++102-Inheritance/main.cpp
+++ 16C++102-Inheritance/16C++102-Inheritance/main.cpp
@@ -119,11 +119,6 @@ public:
 		std::cout << "Base Class Overloaded Constructor Invoked." << std::endl;
 	}
 
-	// ~Base2()
-	// {
-	// 	std::cout << "Base Class Destructor Invoked." << std::endl;
-	// }
-
 protected:
 	int x;
 };
@@ -151,15 +146,92 @@ public:
 
 	void toString() { std::cout << "x: " << x << '\t' << "y: " << y << std::endl; }
 
-	// ~Derived2()
-	// {
-	// 	std::cout << "Derived Class Destructor Invoked." << std::endl;
-	// }
+private:
+	int y;
+};
+
+
+class NBase
+{
+public:
+	NBase(int xP = 1) : x(xP) {}
+
+	void toString()
+	{
+		std::cout << "---Base Class---\nx: " << x << std::endl;
+	}
+
+private:
+	int x;
+};
+
+class NDerived : public NBase
+{
+public:
+	NDerived(int xP = 1, int yP = 2) : NBase(xP), y(yP) {}
+
+	void toString()
+	{
+		//NBase::toString();
+		std::cout << "---Derived Class---" << '\n' << "y: " << y << std::endl;
+		//std::cout << "---Derived Class---\nx: " << x << '\n' << "y: " << y << std::endl;
+	}
 
 private:
 	int y;
 };
 
+
+class Character
+{
+public:
+	Character(int healthP = 100, int armorP = 0)
+		: health(healthP), armor(armorP)
+	{}
+
+	void setStats(int healthP)
+	{
+		health = healthP;
+	}
+
+	void setStats(int healthP, int armorP)
+	{
+		health = healthP;
+		armor = armorP;
+	}
+
+	void printStats()
+	{
+		std::cout << "Health: " << health << "\nArmor: " << armor << std::endl;
+	}
+
+protected:
+	int health;
+	int armor;
+};
+
+class PlayerCharacter : public Character
+{
+public:
+	PlayerCharacter(int healthP = 100, int armorP = 0, int xpP = 0)
+		: Character(healthP, armorP), xp(xpP)
+	{}
+
+	void setStats(int healthP, int armorP, int xpP)
+	{
+		health = healthP;
+		armor = armorP;
+		xp = xpP;
+	}
+
+	void printStats()
+	{
+		std::cout << "Health: " << health << "\nArmor: " << armor << "\nXP: " << xp << std::endl;
+	}
+
+private:
+	int xp;
+};
 
 
 int main()
@@ -199,6 +271,27 @@ int main()
 	// //Derived2 derived2(5);
 	// Derived2 derived2(2, 3);
 	// derived2.toString();
+
+
+
+	// 145. Redefining Base Class Member Functions
+
+	// NBase base(4);
+	// base.toString();
+	// NDerived derived(33, 55);
+	// derived.toString();
+	// derived.NBase::toString();
+	//
+	//
+	// // Character npc;
+	// // npc.setStats(100, 22);
+	// // npc.printStats();
+	// PlayerCharacter player;
+	// player.Character::setStats(2, 2);
+	// player.setStats(200, 100, 1000);
+	// player.Character::printStats();
+	// player.printStats();
+
 
 
 
