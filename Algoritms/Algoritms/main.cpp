54008d80df370dd9452706e8d07195915cecaa89
SiTiROS <sitiros666@gmail.com>
2024-08-01 21:16:09 +0600

    Add algotitms

--- Algoritms/Algoritms/main.cpp
+++ Algoritms/Algoritms/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -77,7 +78,6 @@ void selectionSort(int arr[], int n)
 
 
 
-
 // Уебанское создание, заполнение и вывод 2D массива - пробема с  выводом через указатель
 //const int M_MAXIMAL = 100;
 //void print_array2d(int A[][M_MAXIMAL], int N, int M)
@@ -109,14 +109,306 @@ void print_array2d(int* p, int N, int M)
 
 
 
+// Практика указателей
+int f(int* x)
+{
+	//return *x = *x * 2;
+	return *x *= 2;
+}
+
+void arrayArguments(int* ar, int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << *(ar + i) << ' ';
+	}
+	cout << endl;
+}
+
+void arrayFunc(int* ar, int len)
+{
+	for (int* ptr = ar; ptr < ar + len; ptr++)
+	{
+		cout << *ptr << ' ' << endl;
+		//cout << *(ar + len -1) << ' ' << endl;//смотрю условия -адрес массива + ссдвиг длины - 1 так как с 0 начало
+	}
+	cout;
+}
+
+
+//Пузырьковая сортировка
+bool cmp(int** x, int** y)
+{
+	return **x < **y;
+}
+
+void sort(int* ar, int len)
+{
+	for (int i = 0; i < len - 1; i++)
+	{
+		int itterator = 0;
+		for (int* j = ar; j < ar + len - 1; j++)
+		{
+			itterator++;
+			cout << "i: " << i << "   itterator: " << itterator << endl;
+			cout << "*j: " << *j << endl;
+			int* k = j + 1;
+			if (cmp(&k, &j))
+			{
+				int tmp = *j;
+				*j = *k;
+				*k = tmp;
+			}
+			cout << endl;
+			for (int i = 0; i < 10; i++)
+			{
+				cout << ar[i] << ' ';
+			}
+			cout << endl;
+		}
+	}
+}
+
+// связанный список
+class link
+{
+public:
+	int val;
+	link* next;
+};
+
+class linkList
+{
+private:
+	link* first;
+	link* last;
+
+public:
+	linkList() : first(nullptr), last(nullptr) {}
+
+	bool isEmpty() { return first == nullptr; }
+
+	void pushFront(int x)
+	{
+		link* tmp = new link;
+		tmp->val = x;
+		tmp->next = first;
+		first = tmp;
+		//if(last==nullptr)
+		//{
+		//	last = first;
+		//}
+	}
+
+	void pushBack(int x)
+	{
+		link* tmp = new link;
+		tmp->val = x;
+		tmp->next = nullptr;
+		if (this->isEmpty())
+		{
+			first = last = tmp;
+		}
+		else
+		{
+			last->next = tmp;
+			last = tmp;
+		}
+	}
+
+	void popFront()
+	{
+		link* tmp = first->next;
+		delete first;
+		first = tmp;
+	}
+
+	void popBack()
+	{
+		link* tmp = first;
+		while (tmp->next != last)
+		{
+			tmp = tmp->next;
+		}
+		delete last;
+		last = tmp;
+		last->next = nullptr;
+	}
+
+	void display()
+	{
+		link* tmp = first;
+		while (tmp != nullptr)
+		{
+			cout << tmp->val << endl;
+			tmp = tmp->next;
+		}
+	}
+};
+
+//указатель на функцию ???
+void test() { cout << "Test" << endl; }
+void test2() { cout << "Test2" << endl; }
+
+//
+void add(int x, int y)
+{
+	cout << "x + y = " << x + y << endl;
+}
+
+void substract(int x, int y)
+{
+	cout << "x - y = " << x - y << endl;
+}
+
+void multiply(int x, int y)
+{
+	cout << "x * y = " << x * y << endl;
+}
+
+//
+void select(void(*op)(int, int), int x, int y)
+{
+	op(x, y);
+}
+
+//
+bool cmp2(int x, int y)
+{
+	return x < y;
+}
+
+//
+int add2(int x, int y)
+{
+	return x + y;
+}
+
+int substract2(int x, int y)
+{
+	return x - y;
+}
+
+int multiply2(int x, int y)
+{
+	return x * y;
+}
+
+int (*select(int choice))(int x, int y)
+{
+	switch (choice)
+	{
+	case 2:
+		return substract2;
+	case 3:
+		return multiply2;
+	default:
+		return add2;
+	}
+}
+
 int main()
 {
+	//указатель на функцию ???
+	void (*ptr)();
+	ptr = test;
+	ptr();
+	ptr = test2;
+	ptr();
+
+	//
+	int a = 10;
+	int b = 5;
+	void (*operations[3])(int, int) = { add, substract, multiply };
+
+	for (unsigned i = 0; i < size(operations); i++)
+	{
+		operations[i](a, b);
+	}
+
+	//
+	select(add, a, b);
+
+	//
+	int ar[] = { 4, 3, 5, 7, 1, -12, 5, 15, -15 };
+	sort(ar, ar + size(ar), cmp2);
+	for (int i = 0; i < size(ar); i++)
+	{
+		cout << ar[i] << " ";
+	}
+	cout << endl;
+
+	//
+	int(*action)(int, int) = select(1);
+	cout << action(8, 5) << endl;
+
+	action = select(2);
+	cout << action(8, 5) << endl;
+
+	action = select(3);
+	cout << action(8, 5) << endl;
+
+	//список - но надо переделать и улучшить - дз
+	linkList test;
+	test.pushBack(10);
+	test.pushBack(20);
+	test.pushFront(30);
+	test.popBack();
+	test.popBack();
+	test.popFront();
+	test.display();
+
+
+
+	//Пузырьковая сортировка
+	////int ar[] = { 4, -5, 8, -1, 0, 15, 3, -7, 9, 11 };
+	//int ar[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	//
+	//sort(ar, size(ar));
+	//
+	//cout << size(ar) << endl;
+	//
+	//for (int i = 0; i < size(ar); i++)
+	//{
+	//	cout << ar[i] << ' ';
+	//}
+
+	// practices pointers
+	//int x = 5;
+	//cout << f(&x) << endl;
+	//
+	//int array[] = { 1, 2, 3, 4, 5 };
+	//for (int i = 0; i < size(array); i++)
+	//{
+	//	cout << (array + i) << ' ';
+	//	cout << *(array + i) << ' ';
+	//	cout << endl;
+	//}
+	//
+	//int array[] = { 1, 2, 3, 4, 5 };
+	//arrayArguments(&array[0], size(array));
+	//arrayArguments(array, size(array));
+	//
+	//int array[] = { 1, 2, 3, 4, 5 };
+	//arrayFunc(array, size(array));
+	//
+	//int i;
+	//cin >> i;
+	//int* pAr = new int[i]{0};
+	//arrayFunc(pAr, i);
+	//delete[] pAr;
+	//
+
+
+
+
 	// binarySearch
 	//int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	//int size = sizeof(array) / sizeof(int);
 	//
 	//int index = binarySearch(array, size, 10);
 	//cout << index << endl;
+
 
 
 
