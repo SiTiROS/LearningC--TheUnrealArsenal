4a8e63eca475637f3aff595c4d2c6c7651ebbf8b
SiTiROS <sitiros666@gmail.com>
2024-08-02 04:21:54 +0600

    149. Namespaces

--- 16C++102-Inheritance/16C++102-Inheritance/main.cpp
+++ 16C++102-Inheritance/16C++102-Inheritance/main.cpp
@@ -27,6 +27,56 @@ struct StructD2 : public StructB2, public StructC2
 {};
 
 
+namespace DemoNamespaces
+{
+	int var;
+
+	struct Entity
+	{
+		int member;
+	};
+
+	void hello()
+	{
+		std::cout << "Hello." << std::endl;
+	}
+
+	enum EGameDifficulty
+	{
+		Easy,
+		Normal,
+		Hard
+	};
+}
+
+enum class ETerrainDifficulty
+{
+	Easy,
+	Normal,
+	Hard
+};
+
+int var = 44;
+
+namespace DemoNamespace2
+{
+	void hello2();
+}
+
+namespace ParentNamespace
+{
+	inline namespace NestedNamespace
+	{
+		int var3 = 55;
+
+		void hello() { std::cout << "Hello" << std::endl; }
+	}
+
+	void printVar() { std::cout << var3 << std::endl; }
+}
+
+namespace n = ParentNamespace;
+
 int main()
 {
 	//147. Interface and Implementation Files
@@ -53,7 +103,48 @@ int main()
 
 
 
+	// 149. Namespaces
+
+	//using std::cout;
+	//using std::endl;
+	//
+	//using DemoNamespaces::Entity;
+	////using DemoNamespaces::var;
+	//
+	//int var = 33;
+	//
+	//// DemoNamespaces::var = 5;
+	//DemoNamespaces::var = 5;
+	//::var = 22;
+	//
+	//// DemoNamespaces::Entity entity;
+	//Entity entity;
+	//
+	//entity.member = 55;
+	//
+	//cout << entity.member << endl;
+	//cout << DemoNamespaces::var << endl;
+	//cout << ::var << endl;
+	//cout << var << endl;
+	//
+	//DemoNamespaces::EGameDifficulty difficulty = DemoNamespaces::EGameDifficulty::Hard;
+	//DemoNamespaces::EGameDifficulty difficulty = DemoNamespaces::Hard;
+	//	
+	//DemoNamespaces::hello();
+	//DemoNamespace2::hello2();
+	//
+	ParentNamespace::NestedNamespace::hello();
+	ParentNamespace::printVar();
+
+	n::NestedNamespace::hello();
+	n::printVar();
+
+
 	std::cout << "END" << std::endl;
 	return 0;
 }
 
+namespace DemoNamespace2
+{
+	void hello2() { cout << "Hello2." << endl; }
+}
