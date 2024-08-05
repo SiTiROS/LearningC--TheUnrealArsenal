cdebdea58df853ee0215d07031bda1ba54564254
SiTiROS <sitiros666@gmail.com>
2024-08-06 05:29:02 +0600

    164. Class Templates

--- 18 C++102 - Templates/18 C++102 - Templates/18 C++102 - Templates.cpp
+++ 18 C++102 - Templates/18 C++102 - Templates/18 C++102 - Templates.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <utility>
 
 namespace L162
 {
@@ -72,14 +73,72 @@ namespace L164
 {
 	using namespace std;
 
+	template<typename T>
+	class Entity
+	{
+	public:
+		Entity(T memberP) : member(memberP) {}
 
+		void setMember(T memberP) { member = memberP; }
+
+		T getMember() { return member; }
+
+	private:
+		T member;
+	};
 }
 
-namespace L164
+namespace L164v2
 {
 	using namespace std;
 
+	template<typename T>
+	class Entity
+	{
+	public:
+		Entity(T memberP);
 
+		~Entity();
+
+		void setMember(T memberP);
+
+		T getMember();
+
+	private:
+		T member;
+	};
+
+	template<typename T>
+	Entity<T>::Entity(T memberP) : member(memberP)
+	{
+		cout << "Constructor invoked" << endl;
+	}
+
+	template<typename T>
+	Entity<T>::~Entity()
+	{
+		cout << "Destructor invoked" << endl;
+	}
+
+	template<typename T>
+	void Entity<T>::setMember(T memberP)
+	{
+		member = memberP;
+	}
+
+	template<typename T>
+	T Entity<T>::getMember()
+	{
+		return member;
+	}
+
+	//
+	template<typename T1, typename T2>
+	struct Pair
+	{
+		T1 first;
+		T2 second;
+	};
 }
 
 namespace L165
@@ -90,6 +149,13 @@ namespace L165
 }
 
 namespace L166
+{
+	using namespace std;
+
+
+}
+
+namespace L167
 {
 	using namespace std;
 
@@ -135,19 +201,66 @@ int main()
 
 	// 163. Function Templates with Class or Struct Type Arguments
 	{
-		using namespace L163;
+		//using namespace L163;
 
-		Entity entity1{ 22 };
-		Entity entity2{ 11 };
+		//Entity entity1{ 22 };
+		//Entity entity2{ 11 };
 
-		//Entity entity3 = smallest(entity1, entity2);
-		//cout << entity3.member << endl;
+		////Entity entity3 = smallest(entity1, entity2);
+		////cout << entity3.member << endl;
 
-		cout << smallest<Entity>(entity1, entity2);
+		//cout << smallest<Entity>(entity1, entity2);
 
-		print(entity2);
+		//print(entity2);
 	}
 
+
+	// 164. Class Templates
+	{
+		//using namespace L164;
+
+		//Entity<int> entity1(55);
+
+		//cout << entity1.getMember() << endl;
+
+		//entity1.setMember(3);
+
+		//cout << entity1.getMember() << endl;
+	}
+
+	{
+		using namespace L164v2;
+
+		//Entity<int> entity1(55);
+
+		//cout << entity1.getMember() << endl;
+
+		//entity1.setMember(3);
+
+		//cout << entity1.getMember() << endl;
+
+
+		//
+		//Pair<int, char> pair1{44, 'H'};
+
+		//cout << pair1.first << endl;
+		//cout << pair1.second << endl;
+	}
+
+	{
+		using std::pair;
+
+		pair<char, int> pair1{'H', 55};
+		pair<char, int> pair2{'B', 33};
+
+		std::cout << pair1.first << std::endl;
+		std::cout << pair1.second << std::endl;
+
+		pair1.swap(pair2);
+
+		std::cout << pair1.first << std::endl;
+		std::cout << pair1.second << std::endl;
+	}
 
 	std::cout << "\nEND!\n";
 
