393840a03fe9080958d9cfd19d9c6e7de9a46784
SiTiROS <sitiros666@gmail.com>
2024-09-26 05:40:47 +0600

    182. Predefined Function Objects

---
+++ 20 C++102 - Callables/182. Predefined Function Objects/mian.cpp
@@ -0,0 +1,40 @@
+﻿// 182. Predefined Function Objects
+
+#include <iostream>
+#include <functional>  // https://en.cppreference.com/w/cpp/utility/functional
+
+namespace Example
+{
+	class Square_Functor
+	{
+	public:
+		int operator()(int p) const { return (p * p); }
+	};
+
+	class Addition_Functor
+	{
+	public:
+		int operator()(int lho, int rho) const { return (lho + rho); }
+	};
+}
+
+int main()
+{
+	std::plus<int> addition;
+	std::minus<int> substraction;
+	std::greater<int> greaterThan;
+	std::less<int> lessThan;
+
+	std::cout << "5 + 3: " << addition(5, 3) << std::endl;
+	std::cout << "5 - 3: " << substraction(5, 3) << std::endl;
+	
+	
+	std::cout << std::boolalpha;
+	std::cout << "5 > 3: " << greaterThan(5, 3) << std::endl;
+	std::cout << "5 < 3: " << lessThan(5, 3) << std::endl;
+
+#if 0
+#endif
+	std::cout << "END\n";
+	return 0;
+}
