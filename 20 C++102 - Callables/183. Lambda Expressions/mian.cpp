b020a282469d0c2bd1f3ba22d4eca433fc3a6e63
SiTiROS <sitiros666@gmail.com>
2024-09-26 06:40:16 +0600

    183. Lambda Expressions

---
+++ 20 C++102 - Callables/183. Lambda Expressions/mian.cpp
@@ -0,0 +1,60 @@
+﻿// 183. Lambda Expressions
+
+#include <iostream>
+using namespace std;
+
+int addition(int x, int y) { return (x + y); }
+
+int main()
+{
+#if 0
+	[]() {cout << "Hello" << endl; }; // не вызовется 
+	[]() {cout << "Hello" << endl; }();
+	[] { cout << "Hello" << endl; }();
+
+	int a{ 5 }, b{ 3 };
+	cout << addition(a, b) << endl;
+	cout << [](int x, int y) {return (x + y); }(a, b) << endl;
+
+	auto hi = []() {cout << "Hi" << endl; };
+	hi();
+
+	auto sum = [](int x, int y) {return (x + y); };
+	cout << sum(a, b) << endl;
+	cout << sum(1, 2) << endl;
+
+#endif
+
+#if 0
+	auto div = [](int x, int y) -> double
+	{
+		if (true)
+		{
+			return (double(x) / y);
+		}
+		return (x / y);
+	};
+	cout << div(3, 2) << endl;
+
+#endif
+
+#if 1
+	auto div = [](auto x, auto y)
+	{ return (x / y); };
+
+	cout << div(3, 2) << endl;
+	cout << div(3.5, 2) << endl;
+
+	auto result = [](auto x, auto y)
+	{ return (x / y); }(5.0, 2);
+
+	cout << result << endl;
+
+#endif
+
+
+#if 0
+#endif
+	std::cout << "END\n";
+	return 0;
+}
