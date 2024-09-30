c221ce38a09714309f24b8ce51d1da4fc5bbf693
SiTiROS <sitiros666@gmail.com>
2024-10-01 04:18:41 +0600

    185. Capturing the this Pointer

---
+++ 20 C++102 - Callables/185. Capturing the this Pointer/main.cpp
@@ -0,0 +1,47 @@
+﻿// 185. Capturing the this Pointer
+
+#include <iostream>
+using namespace std;
+
+class Entity
+{
+public:
+	Entity(int memberP = 0)
+		: member(memberP)
+	{
+		cout << "Entity Constructor Invoked. Member is: " << member << endl;
+	}
+
+	~Entity()
+	{
+		cout << "Entity Destructor Invoked. Member was: " << member << endl;
+	}
+
+	void toString()
+	{
+		//[this]() {cout << "From Entity::toString: " << member << endl; }();
+		//[=]() {cout << "From Entity::toString: " << member << endl; }(); // можно захват всего по значению но лучше this
+		auto func = [this]() {cout << "From Entity::toString: " << member << endl; };
+		func();
+	}
+
+	void setMember(int memberP)
+	{
+		member = memberP;
+		//this->member = memberP; // можно и так this тут по умолчанию как бы скрыт
+	}
+
+private:
+	int member;
+};
+
+int main()
+{
+	Entity entity1{ 55 };
+	entity1.toString();
+
+#if 0
+#endif
+	std::cout << "END" << std::endl;
+	return 0;
+}
