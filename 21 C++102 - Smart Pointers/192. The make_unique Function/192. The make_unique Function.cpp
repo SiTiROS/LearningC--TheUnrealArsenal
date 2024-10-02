75352c4a05e72af795a0c57a75572a8df388f8af
SiTiROS <sitiros666@gmail.com>
2024-10-02 09:09:14 +0600

    192. The make_unique Function

---
+++ 21 C++102 - Smart Pointers/192. The make_unique Function/192. The make_unique Function.cpp
@@ -0,0 +1,34 @@
+﻿// 192. The make_unique Function
+
+#include <iostream>
+#include <memory>
+
+template<typename T>
+class Entity
+{
+public:
+	Entity(T memberP = 44) { member = memberP; }
+	~Entity() { std::cout << "Entity Destructor invoked." << std::endl; }
+	void toString() const { std::cout << member << std::endl; };
+
+private:
+	T member;
+};
+
+
+int main()
+{
+	std::unique_ptr<Entity<int>> uEntityPtr1{ new Entity<int>{ 55 } };
+
+	std::unique_ptr<Entity<int>> uEntityPtr2 = std::make_unique<Entity<int>>(22);
+	uEntityPtr2->toString();
+
+	auto uEntityPtr3 = std::make_unique<Entity<int>>(11);
+	uEntityPtr3->toString();
+
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
---
+++ 21 C++102 - Smart Pointers/193. Unique Pointers as Formal Parameters/193. Unique Pointers as Formal Parameters.cpp
@@ -0,0 +1,13 @@
+﻿// 193. Unique Pointers as Formal Parameters
+
+#include <iostream>
+
+int main()
+{
+
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
