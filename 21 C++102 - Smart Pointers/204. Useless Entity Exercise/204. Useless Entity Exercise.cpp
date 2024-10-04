c1509c32bbbe4e5bb6de1ab0c83a838e2d074693
SiTiROS <sitiros666@gmail.com>
2024-10-05 00:37:29 +0600

    204. Useless Entity Exercise

---
+++ 21 C++102 - Smart Pointers/204. Useless Entity Exercise/204. Useless Entity Exercise.cpp
@@ -0,0 +1,32 @@
+﻿// 204. Useless Entity Exercise
+
+#include <iostream>
+
+class UselessEntity
+{
+public:
+	UselessEntity(int memberP = 0) : member(memberP) 
+	{
+		delete this;
+	}
+
+	~UselessEntity()
+	{
+		std::cout << "UselessEntity Destructor Invoked. Member was: " << member << std::endl;
+	}
+
+private:
+	int member;
+};
+
+
+int main()
+{
+	UselessEntity* ptr = new UselessEntity(44);
+	ptr = nullptr;
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
