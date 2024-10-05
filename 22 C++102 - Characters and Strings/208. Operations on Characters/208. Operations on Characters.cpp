94cdbbb98172880113b27ceeb397fdb6e71ef4fc
SiTiROS <sitiros666@gmail.com>
2024-10-06 04:54:12 +0600

    208. Operations on Characters

---
+++ 22 C++102 - Characters and Strings/208. Operations on Characters/208. Operations on Characters.cpp
@@ -0,0 +1,32 @@
+﻿// 208. Operations on Characters
+
+#include <iostream>
+#include <cctype>
+
+int main()
+{
+	char myChar{ '!' };
+	myChar = toupper(myChar);
+
+	if (isalpha(myChar))
+		std::cout << myChar << " is alphabetic." << std::endl;
+
+	if (isupper(myChar))
+		std::cout << myChar << " is a uppercase letter." << std::endl;
+
+	if (islower(myChar))
+		std::cout << myChar << " is a lowercase letter." << std::endl;
+
+	if (ispunct(myChar))
+		std::cout << myChar << " is a punctuation character." << std::endl;
+
+	if (isdigit(myChar))
+		std::cout << myChar << " is a digit." << std::endl;
+
+
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
