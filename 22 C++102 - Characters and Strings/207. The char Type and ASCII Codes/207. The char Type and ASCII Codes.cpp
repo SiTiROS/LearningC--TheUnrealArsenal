bd5f3b65a603528895e9991573ec0365f762da0a
SiTiROS <sitiros666@gmail.com>
2024-10-06 04:33:39 +0600

    22 C++102 - Characters and Strings - 207. The char Type and ASCII Codes

---
+++ 22 C++102 - Characters and Strings/207. The char Type and ASCII Codes/207. The char Type and ASCII Codes.cpp
@@ -0,0 +1,26 @@
+﻿// 207. The char Type and ASCII Codes
+
+#include <iostream>
+
+int main()
+{
+	char myChar{ 'X' };
+	std::cout << "myChar is: " << myChar << std::endl;
+	
+	myChar = 'Y';
+	std::cout << "myChar is: " << myChar << std::endl;
+
+	myChar = 120; //ASCII code
+	std::cout << "myChar is: " << myChar << std::endl;
+	
+	myChar = 89; //ASCII code
+	std::cout << "myChar is: " << myChar << std::endl;
+
+	std::cout << "Code in myChar is: " << static_cast<int>(myChar) << std::endl;
+
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
