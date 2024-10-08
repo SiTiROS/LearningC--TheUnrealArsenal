82e9b4f59c910677193249f33b691a73b5e33087
SiTiROS <sitiros666@gmail.com>
2024-10-08 08:56:33 +0600

    216. Accessing Characters in the Standard String

---
+++ 22 C++102 - Characters and Strings/216. Accessing Characters in the Standard String/216. Accessing Characters in the Standard String.cpp
@@ -0,0 +1,43 @@
+﻿// 216. Accessing Characters in the Standard String
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+#if 0
+	std::string s1{ "Bob" };
+	s1 = "Hello";
+
+	s1[0] = 'M';
+	//s1.at(0) = 'M';
+
+	for (size_t i = 0; i < s1.size(); i++)
+	{
+		//std::cout << s1[i] << std::endl;
+		std::cout << s1.at(i) << std::endl;
+	}
+
+	for (char c : s1)
+	{
+		std::cout << c << " ";
+	}
+#endif
+
+#if 1
+	std::string s1{ "Bob" };
+
+	//s1.front() = 'M';
+
+	char& c = s1.front();
+	c = 'M';
+
+	std::cout << s1.front() << std::endl; // first symbol
+	std::cout << s1.back() << std::endl; // end symbol
+#endif
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
