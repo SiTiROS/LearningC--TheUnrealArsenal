1535465d90d7909a09738e2cef4837dad69134fb
SiTiROS <sitiros666@gmail.com>
2024-10-08 08:02:33 +0600

    213. An Introduction to the Standard String Class - stdstring

---
+++ 22 C++102 - Characters and Strings/213. An Introduction to the Standard String Class - stdstring/213. An Introduction to the Standard String Class - stdstring.cpp
@@ -0,0 +1,24 @@
+﻿// 213. An Introduction to the Standard String Class - stdstring
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string s1{ "Hello Human!" };
+	std::string s2{ "Hello Human!", 5 };
+	std::string s3(3, 'X');  // XXX
+	std::string s4{ s1, 0, 5 }; // Hello
+	std::string s5 = s1;
+
+	std::string s6{"Bob"};
+	s6 = s1;
+	s6 = "Hello Human!";
+
+	std::cout << s6 << std::endl;
+
+#if 0
+#endif
+	std::cout << "\n--- End of Program ---" << std::endl;
+	return 0;
+}
